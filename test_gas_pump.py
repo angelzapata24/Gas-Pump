b6837bd8bc189fa8f7029054f6391020e0ee5083
Angel Zapata <azapata@basecampcodingacademy.org>
2017-07-25 08:33:21 -0500

    tested my functions that i have in my gas_pump_core

---
+++ test_gas_pump.py
@@ -0,0 +1,19 @@
+import gas_pump_core
+
+def test_gas_pump():
+    assert gas_pump_core.get_gas_type('1') == 'Regular'
+
+def test_gas_price():
+    assert gas_pump_core.gas_price('one') == '1.88'
+
+def test_gas_pump2():
+    assert gas_pump_core.get_gas_type('2') == 'Midgrade'
+
+def test_gas_price2():
+    assert gas_pump_core.gas_price('two') == '2.26'
+
+def test_gas_pump():
+    assert gas_pump_core.get_gas_type('3') == 'Premium'
+
+def test_gas_price():
+    assert gas_pump_core.gas_price('three') == '1.88'
