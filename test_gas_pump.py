bf4f1f6852336d6ecf5e3f01f757f50b8ec00e0b
Angel Zapata <azapata@basecampcodingacademy.org>
2017-07-25 09:09:15 -0500

    Ran all my test and they passed

--- test_gas_pump.py
+++ test_gas_pump.py
@@ -4,16 +4,16 @@ def test_gas_pump():
     assert gas_pump_core.get_gas_type('1') == 'Regular'
 
 def test_gas_price():
-    assert gas_pump_core.gas_price('one') == '1.88'
+    assert gas_pump_core.gas_price('one', '1') == 1.88
 
 def test_gas_pump2():
     assert gas_pump_core.get_gas_type('2') == 'Midgrade'
 
 def test_gas_price2():
-    assert gas_pump_core.gas_price('two') == '2.26'
+    assert gas_pump_core.gas_price('two', '1') == 2.26
 
-def test_gas_pump():
+def test_gas_pump3():
     assert gas_pump_core.get_gas_type('3') == 'Premium'
 
-def test_gas_price():
-    assert gas_pump_core.gas_price('three') == '1.88'
+def test_gas_price3():
+    assert gas_pump_core.gas_price('three', '1') == 2.89
